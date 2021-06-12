36b26ab97ec49c805b9f9b373251bcfa082d03e7
ntokozogcp <ntokozogcp@gmail.com>
2021-06-12 19:17:42 +0200

    fixed container

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
-import kubernetes import client, config
+from kubernetes import client, config
 
 
 
